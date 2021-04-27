75a56c5b7158cc60e0665cb1aec93e10e30dcb1c
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2021-04-27 06:26:59 -0700

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+remark.create();
+MathJax.Hub.Config({
+    tex2jax: {
+        skipTags: [
+            "script",
+            "noscript",
+            "style",
+            "textarea",
+            "pre",
+        ],
+        inlineMath: [['$','$'], ['\\(','\\)']]
+    },
+});
+MathJax.Hub.Configured();
